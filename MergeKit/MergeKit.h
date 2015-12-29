e6d3f0c0f52c396d10b325cbf6fc8ca32140fd8e
Fernando Rodriguez <frr@agbo.biz>
2015-12-29 10:27:42 -0800

    Initial Commit

---
+++ MergeKit/MergeKit.h
@@ -0,0 +1,19 @@
+//
+//  MergeKit.h
+//  MergeKit
+//
+//  Created by Fernando Rodríguez Romero on 29/12/15.
+//  Copyright © 2015 keepcoding.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MergeKit.
+FOUNDATION_EXPORT double MergeKitVersionNumber;
+
+//! Project version string for MergeKit.
+FOUNDATION_EXPORT const unsigned char MergeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MergeKit/PublicHeader.h>
+
+
